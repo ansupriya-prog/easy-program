ca4fec9b90f6ae45a17e9129508f61b1aa3d81e0
ansupriya <ansupriyarajesh@gmail.com>
2025-08-24 11:39:57 +0530

    Add files via upload

---
+++ check.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter the number :";
+    cin>>a;
+    if(a==0)
+    {
+        cout <<"the number is zero";
+
+    }
+    else if(a>0)
+    {
+        cout <<"the number is a positive number ";
+
+    }
+    else if(a<0)
+    {
+        cout <<"the enterd number is negative";
+    }
+    return 0;
+
+}
