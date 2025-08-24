e5cd2fd9e25c9ebbc557d60d55c389afc27b31d1
ansupriya <ansupriyarajesh@gmail.com>
2025-08-24 11:11:00 +0530

    Add files via upload

---
+++ celsius to fahrenheit.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int c,f;
+    cout<<"enter the celsius to be converted :";
+    cin >> c;
+    f = (c * 9/5)+32;
+    cout<<" celsius is converted to fahrenheit"<< f <<  "F";
+
+}
