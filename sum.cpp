82492f3fce28a51e91cb01b710e5cc24e695636f
ansupriya <ansupriyarajesh@gmail.com>
2025-08-24 10:37:07 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout <<"enter the first number :";
+    cin >>a;
+    cout <<"enter the second number :";
+    cin >>b;
+    int sum=a+b;
+    cout <<"the sum of two number is\n" <<sum;
+return 0;
+}
