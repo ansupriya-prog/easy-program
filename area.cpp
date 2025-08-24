55e33e610a6f812aa4bad1c9141432f46482001b
ansupriya <ansupriyarajesh@gmail.com>
2025-08-24 10:56:02 +0530

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int len,bre;
+    cout <<"enter the length :";
+    cin >> len;
+    cout <<"enter the breadth :";
+    cin >> bre;
+    int area;
+    area=len *bre;
+    cout<<"area of the rectangle is "<<area<< "cm ^2";
+
+return 0;
+}
