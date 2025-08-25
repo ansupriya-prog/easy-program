ee36187615c9b1f87870c16722a7dade2664019c
ansupriya <ansupriyarajesh@gmail.com>
2025-08-25 20:35:46 +0530

    Add files via upload

---
+++ large.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the first  numbers :";
+    cin>>a;
+    cout<<"enter the second number :";
+    cin>>b;
+    if(a<b)
+    {
+        cout<<"the largest number is: "<<b;
+
+    }
+    else
+    {
+        cout<<"the largest number is:"<<a;
+    }
+return 0;
+
+}
