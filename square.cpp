a2856a5b9d87794f5d47dd24bf833c1f2c8ced4f
ansupriya <ansupriyarajesh@gmail.com>
2025-08-25 20:46:29 +0530

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"number  ";
+    cin>>a;
+    int square=a*a;
+    cout<<"the square of " << a << "  is " <<square;
+return 0;
+}
