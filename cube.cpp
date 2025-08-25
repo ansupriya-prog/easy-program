025e8599e1c58815efb41f2fa200a69d16ff3bfc
ansupriya <ansupriyarajesh@gmail.com>
2025-08-25 20:50:36 +0530

    Add files via upload

---
+++ cube.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter the number :";
+    cin >>a;
+    int cube= a*a*a;
+    cout<<"the cube of" <<a<<"  is "<<cube;
+return 0;
+}
