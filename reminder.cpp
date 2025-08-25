6a194afc3c88576307c5d9779f8226bcdcd556ff
ansupriya <ansupriyarajesh@gmail.com>
2025-08-25 18:18:03 +0530

    Add files via upload

---
+++ reminder.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,d,r;
+    cout <<"enter the number to be dividend :";
+    cin >> n;
+    cout <<"enter the  divisor :";
+    cin >> d;
+    r= n%d;
+    cout <<"the reminder is :"<<r;
+return 0;
+
+}
