b892684f68486dea920a5f88a8ba7b89d5f95a9b
ansupriya <ansupriyarajesh@gmail.com>
2025-08-25 18:48:02 +0530

    Add files via upload

---
+++ avg.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter the  3 numbers";
+    cin>>a>>b>>c;
+    int avg=(a+b+c)/3;
+    cout<<"the avg  of three number is "<<avg;
+return 0;
+}
