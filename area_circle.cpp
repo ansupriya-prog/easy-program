373c996fc4489bca81242897487fcf3c03979dcf
ansupriya <ansupriyarajesh@gmail.com>
2025-08-25 20:54:41 +0530

    Add files via upload

---
+++ area_circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r;
+    cout<<"enter the radius of the circle  :";
+    cin>>r;
+    float area =3.14 *r*r;
+    cout<<"the area of the circle is "<<area ;
+return 0;
+}
