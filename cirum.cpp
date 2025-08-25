008961a3e3efca4a02277dc975777942eaa31c5a
ansupriya <ansupriyarajesh@gmail.com>
2025-08-25 21:14:36 +0530

    Add files via upload

---
+++ cirum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r;
+    cout<<"enter the radius :";
+    cin>>r;
+    float c= 2*r*3.14;
+    cout<<"the cirumference of the circle is :"<<c;
+return 0;
+}
