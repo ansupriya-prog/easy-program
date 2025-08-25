c8d862f15acdb9225dc41a8f50891ae75b5dd61f
ansupriya <ansupriyarajesh@gmail.com>
2025-08-25 20:41:34 +0530

    Add files via upload

---
+++ small.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the two numbers";
+    cin>>a>>b;
+    if(a<b)
+    {
+        cout<<"the smallest number is"<<a;
+    }
+    else
+    {
+        cout<<"the smallest number is "<<b;
+    }
+return 0;
+}
