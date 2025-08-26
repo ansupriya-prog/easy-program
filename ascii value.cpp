690daa2113329d673a3de4c24839f25f1ecfd8f2
ansupriya <ansupriyarajesh@gmail.com>
2025-08-26 20:47:22 +0530

    Add files via upload

---
+++ ascii value.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout<<"enter the character ";
+    cin>>c;
+    int asscivalue=c;
+    cout<<"the ascii value of  is"<<asscivalue;
+return 0;
+}
