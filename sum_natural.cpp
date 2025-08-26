7c3f787cefc5224a03118fa852a9c6d06bcab38e
ansupriya <ansupriyarajesh@gmail.com>
2025-08-26 21:45:35 +0530

    Add files via upload

---
+++ sum_natural.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a , sum=0;
+    cout<<"enter the numbers(10) \n :";
+    for(int i=1;i<=10;i++)
+    {
+        cin>>a;
+        sum += a;
+    }
+
+
+
+    cout<<"the sum of the 10 natrual numbers is "<< sum;
+return 0;
+}
+
