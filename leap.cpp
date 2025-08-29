2a8652f1edcf50956ac0593f6e82590edd6239f0
ansupriya <ansupriyarajesh@gmail.com>
2025-08-29 19:11:19 +0530

    Add files via upload

---
+++ leap.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the year :";
+    cin>>n;
+    if(n%4==0)
+    {
+        cout<<"the year is leap";
+    }
+    else
+    {
+        cout<<"the year is not";
+    }
+return 0;
+}
