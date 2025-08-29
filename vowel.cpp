38770af22cb8ee2ed8e4a15e2294b5618a13acfe
ansupriya <ansupriyarajesh@gmail.com>
2025-08-29 18:46:53 +0530

    Add files via upload

---
+++ vowel.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout<<"enter the character ";
+    cin>>c;
+    if(c =='A'|| c=='E'||c=='I'||c=='O'||c=='U'||c=='a' || c=='e'|| c=='i' ||c=='o'||c=='u')
+    {
+        cout<<"the character is vowel";
+
+
+    }
+    else
+    {
+        cout<<"the character is consonan";
+    }
+return 0;
+}
