b85cb334e79a6df011658329d09c834f187d8d7c
ansupriya <ansupriyarajesh@gmail.com>
2025-08-29 19:35:51 +0530

    Add files via upload

---
+++ checkdigit.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int ch;
+    cout<<"enter  :";
+    cin>>ch;
+    if(ch>0&&ch<9)
+    {
+        cout<<"digit";
+    }
+    else
+    {
+        cout<<"not digit";
+    }
+return 0;
+}
