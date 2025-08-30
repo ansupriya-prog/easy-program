9b54a3b103979b36789560c5e4ee9910642645c2
ansupriya <ansupriyarajesh@gmail.com>
2025-08-30 21:24:08 +0530

    Add files via upload

---
+++ day.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the number  :";
+    cin>>n;
+    switch(n)
+    {
+
+
+    case 1 :
+
+            cout<<"the day is monday";
+            break;
+
+    case 2 :
+
+            cout<<"the day is tuesday";
+            break;
+
+    case 3 :
+
+            cout<<"the day is wendsday";
+            break;
+
+    case 4 :
+
+            cout<<"the day is thursday";
+            break;
+
+    case 5 :
+
+            cout<<"the day is friday";
+            break;
+
+    case 6 :
+
+            cout<<"the day is saturday";
+            break;
+
+
+    case 7 :
+
+            cout<<"the day is sunday";
+            break;
+
+    default :
+
+
+            cout<<"invalid option";
+
+    }
+return 0;
+}
