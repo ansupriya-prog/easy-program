760ed3f9e53e5f117d702f57b8dfe41bd4768619
ansupriya <ansupriyarajesh@gmail.com>
2025-09-08 18:23:09 +0530

    Add files via upload

---
+++ electricity.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the unit :";
+    cin>>n;
+    int unit=n/n;
+     int aunit = unit *3;
+    int bunit=aunit*100;
+return 0;
+}
