44c16d61806aabe3a3c7a31dc60ea051bd69ee3f
ansupriya <ansupriyarajesh@gmail.com>
2025-09-08 18:34:03 +0530

    Update electricity.cpp

--- electricity.cpp
+++ electricity.cpp
@@ -1,12 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int n;
-    cout<<"enter the unit :";
-    cin>>n;
-    int unit=n/n;
-     int aunit = unit *3;
-    int bunit=aunit*100;
-return 0;
+
+int main() {
+    float units, bill;
+
+    cout << "Enter electricity units consumed: ";
+    cin >> units;
+
+    if (units <= 100) {
+        bill = units * 3.0;
+    } else if (units <= 200) {
+        bill = (100 * 3.0) + (units - 100) * 4.5;
+    } else if (units <= 300) {
+        bill = (100 * 3.0) + (100 * 4.5) + (units - 200) * 6.0;
+    } else {
+        bill = (100 * 3.0) + (100 * 4.5) + (100 * 6.0) + (units - 300) * 7.5;
+    }
+
+    cout << "Total Electricity Bill = "<< bill << endl;
+
+    return 0;
 }
